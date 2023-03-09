cdda9e99235ac8988b8d28530603b339a5b09b9f
Yuguang Yang <yangyutu123@gmail.com>
2023-03-08 18:22:58 -0800

    adjust the hard negative mining technique to ensure that it is harder than the BM25 hard negatives

--- tasks/retrieval_hard_neg/prepare_hard_neg/build_passage_hard_triplet_crossencoder_train_data.py
+++ tasks/retrieval_hard_neg/prepare_hard_neg/build_passage_hard_triplet_crossencoder_train_data.py
@@ -4,7 +4,7 @@ import os
 import pickle
 import random
 from argparse import ArgumentParser
-from collections import defaultdict
+from collections import defaultdict, deque
 
 from tqdm import tqdm
 from transformers import AutoTokenizer
@@ -20,9 +20,9 @@ import json
 from multiprocessing.dummy import Pool as ThreadPool
 
 
-def _load_embedding_data(args):
+def _load_embedding_data(embedding_path):
 
-    with open(args.query_embedding_path, "rb") as file:
+    with open(embedding_path, "rb") as file:
         embeddings = pickle.load(file)
 
     print(f"load query {len(embeddings)} !")
@@ -43,7 +43,7 @@ def _load_rel(rel_path):
 # multi-thread parallel via multiprocessing.dummy
 # https://stackoverflow.com/questions/26432411/multiprocessing-dummy-in-python-is-not-utilising-100-cpu
 # https://github.com/facebookresearch/faiss/issues/924
-def search_from_index_mp(args, qids_set):
+def search_from_index_mp(args, qids_set, query_embeddings):
 
     print(f"start retrievaling for {len(qids_set)} queries")
     index = faiss.read_index(args.index_save_path)
@@ -51,22 +51,20 @@ def search_from_index_mp(args, qids_set):
     if args.nprobe > 0:
         index.nprobe = args.nprobe
 
-    query_embeddings = _load_embedding_data(args)
-
     query_embeddings = {qid: query_embeddings[qid] for qid in qids_set}
 
     print(f"loading query embeddings of {len(query_embeddings)} queries")
     query_embeddings_np = np.array(list(query_embeddings.values()))
 
     def _search(i):
-        dis, ind = index.search(query_embeddings_np[i : i + 1], args.topk)
+        dot_res, ind = index.search(query_embeddings_np[i : i + 1], args.topk)
         return ind[0].tolist()
 
     pool = ThreadPool(args.num_proc)
     retrieval_results = {}
     search_results = pool.map(_search, range(len(query_embeddings_np)))
     for qid_key, result in zip(query_embeddings.keys(), search_results):
-        result = [str(res) for res in result]
+        result = [str(pid) for pid in result]
         retrieval_results[qid_key] = result
 
     os.makedirs(os.path.dirname(args.retrival_output_path), exist_ok=True)
@@ -110,6 +108,7 @@ def build_training_data(args, triplets):
 
     with open(os.path.join(args.output_dir, "qid_2_query_text.pkl"), "wb") as file:
         pickle.dump(qid_2_query, file)
+
     with open(os.path.join(args.output_dir, "pid_2_passage_text.pkl"), "wb") as file:
         pickle.dump(pid_2_passage, file)
 
@@ -133,7 +132,9 @@ def read_triplets(args):
     return triplets, distinct_qids
 
 
-def gen_static_hardnegs(args, retrieval_results, triplets):
+def gen_static_hardnegs(
+    args, retrieval_results, triplets, query_embeddings, doc_embeddings
+):
     rel_dict = _load_rel(args.qrel_path)
 
     # remove false negative
@@ -143,14 +144,37 @@ def gen_static_hardnegs(args, retrieval_results, triplets):
         retrieval_results[k] = v
 
     new_triplets = []
+    existing_dot_values = []
+    new_dot_values = []
+    replacement_count = 0
     for triplet in tqdm.tqdm(triplets):
         qid, pid_pos, pid_neg = triplet
 
-        if retrieval_results[qid]:
-            hard_neg = retrieval_results[qid].pop()
-            new_triplets.append((qid, pid_pos, hard_neg))
+        existing_dot_value = np.sum(query_embeddings[qid] * doc_embeddings[pid_neg])
+        existing_dot_values.append(existing_dot_value)
+
+        ntrial = len(retrieval_results[qid])
+        candidates = retrieval_results[qid]
+        # If we shuffle the candidates, we can generate harder negatives
+        # random.shuffle(candidates)
+        candidates = deque(candidates)
+        while candidates and ntrial > 0:
+            hard_neg_pid = candidates.pop()
+            new_dot_value = np.sum(query_embeddings[qid] * doc_embeddings[hard_neg_pid])
+            if new_dot_value > existing_dot_value:
+                new_triplets.append((qid, pid_pos, hard_neg_pid))
+                new_dot_values.append(new_dot_value)
+                replacement_count += 1
+                break
+            else:
+                candidates.appendleft(hard_neg_pid)
+            ntrial -= 1
 
     print(f"there are {len(new_triplets)} new triplets in the training data")
+    print(
+        f"there are {replacement_count} replacements: old dot value {np.mean(np.array(existing_dot_values))}, new dot value {np.mean(np.array(new_dot_values))}"
+    )
+
     with open(os.path.join(args.output_dir, "triplets.pkl"), "wb") as file:
         pickle.dump(new_triplets, file)
     print(f"done triplets!")
@@ -163,6 +187,7 @@ if __name__ == "__main__":
     parser.add_argument("--passage_collection", required=True)
     parser.add_argument("--query_collection", required=True)
     parser.add_argument("--query_embedding_path", type=str, required=True)
+    parser.add_argument("--doc_embedding_path", type=str, required=True)
     parser.add_argument("--qrel_path", type=str, required=True)
     parser.add_argument("--topk", type=int, required=True)
     parser.add_argument("--index_save_path", required=True)
@@ -171,13 +196,20 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", type=str)
     parser.add_argument("--num_proc", type=int, default=24)
     parser.add_argument("--name_tag", type=str, default="")
-    args = parser.parse_args()
+    parser.add_argument("--seed", type=int, default=1)
 
+    args = parser.parse_args()
+    random.seed(args.seed)
     print(args)
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
     triplets, distict_qids = read_triplets(args)
-    retrieval_results = search_from_index_mp(args, distict_qids)
-    hard_triplets = gen_static_hardnegs(args, retrieval_results, triplets)
+    query_embeddings, doc_embeddings = _load_embedding_data(
+        args.query_embedding_path
+    ), _load_embedding_data(args.doc_embedding_path)
+    retrieval_results = search_from_index_mp(args, distict_qids, query_embeddings)
+    hard_triplets = gen_static_hardnegs(
+        args, retrieval_results, triplets, query_embeddings, doc_embeddings
+    )
     build_training_data(args, hard_triplets)
--- tasks/retrieval_hard_neg/prepare_hard_neg/run_build_hard_triplets_data.sh
+++ tasks/retrieval_hard_neg/prepare_hard_neg/run_build_hard_triplets_data.sh
@@ -1,11 +1,13 @@
-
+doc_embed_path=./experiments/precomputed_embeddings/hard_neg_prepare/doc_embed.pkl
+query_embed_path=./experiments/precomputed_embeddings/hard_neg_prepare/query_embed.pkl
 msmarco_data_root=/mnt/d/MLData/data/msmarco_passage
 
 python tasks/retrieval_hard_neg/prepare_hard_neg/build_passage_hard_triplet_crossencoder_train_data.py \
---triplet_file ${msmarco_data_root}/triplets/qidpidtriples.train.small.2.tsv \
+--triplet_file ${msmarco_data_root}/triplets/qidpidtriples.train.tiny.2.tsv \
 --passage_collection ${msmarco_data_root}/collection.tsv \
 --query_collection ${msmarco_data_root}/queries.train.tsv \
---query_embedding_path ./experiments/precomputed_embeddings/hard_neg_prepare/query_embed.pkl \
+--query_embedding_path ${query_embed_path} \
+--doc_embedding_path ${doc_embed_path} \
 --qrel_path ${msmarco_data_root}/qrels.train.tsv \
 --index_save_path ./experiments/index/hard_neg_prepare/ivf_nlist_1000.index \
 --nprobe 30 \
